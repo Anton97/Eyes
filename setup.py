1fec3c420ed1b549374d1046368dc3582758f452
Anton97 <Antonbuivol@mail.ru>
2025-06-18 06:40:48 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
-
 from setuptools import setup, find_packages
 
 setup(
-    name='resnet34lib',
-    version='0.1',
+    name='pupil_analysis_package',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'torch',
-        'torchvision'
+        'opencv-python',
+        'numpy',
+        'pandas',
+        'tqdm',
+        'scikit-learn',
+        'joblib',
     ],
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A simple library that provides ResNet34 model.',
-    url='https://your-repo-url.com',
+    author='Anton Krasnoyarov',
+    description='A Python library for pupil analysis from video files.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Anton97/Eyes',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License'
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
+    python_requires='>=3.8',
 )
+
+
